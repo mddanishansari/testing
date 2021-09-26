e9356c25dc5d5498704c5428ec2308f251e50e3c
MD Danish Ansari <ansarid567@gmail.com>
2021-09-26 13:51:52 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String someKey = System.getenv("SOME_KEY");
+        if(someKey == null) {
+            someKey = "key not found";
+        }
+        System.out.println(someKey);
+    }
+}
