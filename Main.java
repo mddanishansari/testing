46458bdbf20df3db8ab0f1a3f799c30c52446e93
MD Danish Ansari <ansarid567@gmail.com>
2021-09-26 13:57:41 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String someKey = System.getenv("SOME_KEY");
-        if(someKey == null) {
-            someKey = "key not found";
-        }
-        System.out.println(someKey);
-    }
-}
